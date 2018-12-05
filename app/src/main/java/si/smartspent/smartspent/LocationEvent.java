84acf0850ccbf84154aed9a7a537bf95bc7ae28d
Zoran Altiparmakov <zoran.altiparmakov@gmail.com>
2018-12-05 02:10:52 +0100

    Created Location Service using GPS and Network provider to listen on location change. Location Event used by EventBus to send events to subscribers on location change.

---
+++ app/src/main/java/si/smartspent/smartspent/LocationEvent.java
@@ -0,0 +1,22 @@
+package si.smartspent.smartspent;
+
+import android.location.Location;
+
+import com.google.android.gms.location.places.PlaceLikelihood;
+
+public class LocationEvent {
+    Location location;
+
+    public LocationEvent(Location location) {
+        this.location = location;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    @Override
+    public String toString() {
+        return location.getLongitude() + " " + location.getLongitude();
+    }
+}
---
+++ app/src/main/java/si/smartspent/smartspent/LocationService.java
@@ -0,0 +1,114 @@
+package si.smartspent.smartspent;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+
+import org.greenrobot.eventbus.EventBus;
+
+public class LocationService extends Service {
+    private static final String TAG = LocationService.class.getName();
+    private LocationManager mLocationManager = null;
+    private static final int LOCATION_INTERVAL = 1000;
+    private static final float LOCATION_DISTANCE = 10f;
+
+    // enable GPS and Network Provider location listeners
+    LocationListener[] mLocationListeners = new LocationListener[] {
+            new LocationListener(LocationManager.GPS_PROVIDER),
+            new LocationListener(LocationManager.NETWORK_PROVIDER)
+    };
+
+    private class LocationListener implements android.location.LocationListener {
+        Location mLastLocation;
+
+        public LocationListener(String provider) {
+            mLastLocation = new Location(provider);
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            EventBus.getDefault().post(new LocationEvent(location));
+            mLastLocation.set(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onCreate() {
+        if(mLocationManager == null) {
+            mLocationManager = (LocationManager) getApplication().getSystemService(Context.LOCATION_SERVICE);
+        }
+
+        try {
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    LOCATION_INTERVAL,
+                    LOCATION_DISTANCE,
+                    mLocationListeners[0]
+            );
+        } catch (SecurityException e) {
+            Log.i(TAG, "fail to request location update. Ignoring..");
+        } catch (IllegalArgumentException e) {
+            Log.d(TAG, "network provider does not exist, " + e.getMessage());
+        }
+
+        try {
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    LOCATION_INTERVAL,
+                    LOCATION_DISTANCE,
+                    mLocationListeners[1]
+            );
+        } catch (SecurityException e) {
+            Log.i(TAG, "failed to request location update. Ignoring..");
+        } catch (IllegalArgumentException e) {
+            Log.d(TAG, "network provider does not exist, " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if(mLocationManager != null) {
+            for(int i = 0; i < mLocationListeners.length; i++) {
+                try {
+                    mLocationManager.removeUpdates(mLocationListeners[i]);
+                } catch (Exception e) {
+                    Log.i(TAG, "failed to remove location listeners. Ignoring..");
+                }
+            }
+        }
+    }
+}
