c497f5c6d99644db0093b1be42b3ba2b75b4205d
Uroš Marković <uros.markovic@student.um.si>
2019-02-04 23:28:14 +0100

    Last Changes: -Tried a adding GA for estimating users spending for next month -Fixed incorrect points on ProfileActivity

--- app/src/main/java/si/smartspent/smartspent/Profile/PointsView.java
+++ app/src/main/java/si/smartspent/smartspent/Profile/PointsView.java
@@ -4,54 +4,119 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.os.AsyncTask;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import si.smartspent.smartspent.R;
 
 import static android.content.Context.WINDOW_SERVICE;
+import static org.greenrobot.eventbus.EventBus.TAG;
+import static si.smartspent.smartspent.Utils.API_URL;
 
 public class PointsView extends View {
-    private int points;
+    int points;
     Bitmap bitmap, oldBitmap;
+    org.json.simple.JSONObject jsonObjectsimple;
 
     public PointsView(Context context) {
         super(context);
-        // points = Utils.getPoints(this);
-        points = 9000;
+        try {
+            JSONObject jsonObject = new JSONObject(jsonObjectsimple.toString());
+            points = jsonObject.getInt("points");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public PointsView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        // points = Utils.getPoints(this);
-        points = 9000;
+        try {
+        JSONObject jsonObject = new JSONObject(jsonObjectsimple.toString());
+        points = jsonObject.getInt("points");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public PointsView(Context context, AttributeSet attrs, int defaultStyle) {
         super(context, attrs, defaultStyle);
-        // points = Utils.getPoints(this);
-        points = 9000;
+        try {
+        JSONObject jsonObject = new JSONObject(jsonObjectsimple.toString());
+        points = jsonObject.getInt("points");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class UserPointsDataTask extends AsyncTask<Void, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            final OkHttpClient client;
+
+            try {
+                OkHttpClient.Builder builder = new OkHttpClient.Builder();
+                client = builder.build();
+
+                Request request = new Request.Builder()
+                        .header("Content-Type", "application/json")
+                        .url(API_URL + "/user")
+                        .get()
+                        .build();
+
+                Response response = client.newCall(request).execute();
+
+                if (response.isSuccessful()) {
+                    String jsonString = response.body().string();
+                    // parse JSON array string with transactions to a JSONArray
+                    jsonObjectsimple = (org.json.simple.JSONObject) (new JSONParser()).parse(jsonString);
+                    return true;
+                }
+            } catch (IOException e) {
+                Log.e(TAG, "Could not execute HTTP request.");
+            } catch (org.json.simple.parser.ParseException e) {
+                Log.e(TAG, "Error while parsing JSON string.");
+            }
+            return false;
+        }
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) { // TODO Maybe add some leaderboard type ordeal?
+    public boolean onTouchEvent(MotionEvent event) {
         float eventX = event.getX();
         float eventY = event.getY();
-        Toast.makeText(getContext(), "X: " + eventX + " Y: " + eventY + points, Toast.LENGTH_SHORT).show();
+        Toast.makeText(getContext(), "Points: " + points, Toast.LENGTH_SHORT).show();
         return true;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         int l = 0;
-        int p = 500;
+        int p = 10;
         int x = 0;
 
-        // TODO Animate pulsating effect over the icons
+        new PointsView.UserPointsDataTask().execute((Void) null);
+        try {
+            JSONObject jsonObject = new JSONObject(jsonObjectsimple.toString());
+            points = jsonObject.getInt("points");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         Display display = ((WindowManager) getContext().getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
         int screenWidth = display.getWidth();
         int screenHeight = display.getHeight();
@@ -70,7 +135,7 @@ public class PointsView extends View {
         oldBitmap.recycle();
         while(l < 5) {
             if(points >= p) {
-                p += 500;
+                p += 10;
                 canvas.drawBitmap(bitmap, x, 0, null);
                 x += size + (size * 0.25f);
             }
--- app/src/main/java/si/smartspent/smartspent/Profile/ProfileActivity.java
+++ app/src/main/java/si/smartspent/smartspent/Profile/ProfileActivity.java
@@ -1,6 +1,7 @@
 package si.smartspent.smartspent.Profile;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -10,7 +11,12 @@ import si.smartspent.smartspent.CustomViews.CircleImageView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 import si.smartspent.smartspent.DrawerActivity;
+import si.smartspent.smartspent.GeneticAlgorithm.Chromosome;
 import si.smartspent.smartspent.R;
 import si.smartspent.smartspent.Utils;
 
@@ -28,6 +34,9 @@ public class ProfileActivity extends DrawerActivity {
     private PointsView pView;
     private TextView points;
 
+    private TextView monthSpent;
+    private TextView estSpending;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +48,9 @@ public class ProfileActivity extends DrawerActivity {
         birth_date = (TextView) findViewById(R.id.birth_date);
         country = (TextView) findViewById(R.id.country);
         avatar = (CircleImageView) findViewById(R.id.avatar);
+
+        monthSpent = (TextView) findViewById(R.id.tV_spent_this_month);
+        estSpending = (TextView) findViewById(R.id.tV_estimated_spending_next_month);
 
         // Get user data from Shared Preferences
         username.setText(Utils.getUserData(this, "username"));
@@ -61,5 +73,9 @@ public class ProfileActivity extends DrawerActivity {
         tabLayout.setupWithViewPager(mViewPager);
         pView = (PointsView) findViewById(R.id.pointsView);
         points = (TextView) findViewById(R.id.textView_points);
+    }
+
+    private void estimateSpending() {
+
     }
 }
